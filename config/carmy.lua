eb505d196df025f1a7358b251612ae6535aa263b
Oreshka23 <33371858+Oreshka23@users.noreply.github.com>
2018-05-30 17:04:07 +0800

    Delete carmy.lua

--- config/carmy.lua
+++
@@ -1,7 +0,0 @@
-carmy = {}
-carmy.tagF = "СВ |" -- Тэг для /f рации
-carmy.tagR = "[USA]" -- Тэг для /r рации
-carmy.interval = 10 -- интервал для докладов с постов в минутах.
-carmy.repeatt = 1 -- Сколько раз повторять доклад
-carmy.autoscreen = "{008000}ON{FFFFFF}" -- Делать ли скриншот после доклада
-carmy.post = "Вышка-1" -- Пост
